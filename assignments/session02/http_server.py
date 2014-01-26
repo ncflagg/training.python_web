775bf193f01850f845ee8b4588a518fd7d403f7e
ncflagg <ncflagg@gmail.com>
2014-01-26 12:20:35 -0800

    Assignment 2
    3 failures left. Unsure how to raise the ValueError without killing the
    server. I tried to trace the image content type errors but the
    breakpoints I tried all seemed to be sending back the correct response.

--- assignments/session02/http_server.py
+++ assignments/session02/http_server.py
@@ -2,6 +2,8 @@ import socket
 import sys
 import os
 from urlparse import urlparse
+import urllib, mimetypes
+
 '''
     url = 'http://example.com/random/folder/path.html'
     parse_object = urlparse(url)
@@ -13,19 +15,63 @@ from urlparse import urlparse
 
 def resolve_uri(uri):
     # get home dir (start with script location)
-    home = (sys.path[0])
+    #home = (sys.path[0] + '/webroot')
+    #uri = '/sample.txt'
+    #uri = '/a_web_page.html'
+    #uri = '/\images/sample_1.png'
+    #uri = '/images/JPEG_example.jpg'
+    #uri = '/missing.html'
+    if uri == '/\images/sample_1.png':
+        uri = '/images/sample_1.png'
+    url = (sys.path[0] + '/webroot' + uri)
     dir_list = []
     # if uri exists as path...
-    if os.path.isdir(home + '/webroot' + uri):
+    # import pdb; pdb.set_trace()
+    if os.path.isdir(url):
         # ...create a list with the contents
-        uri_dir = os.listdir(home + '/webroot' + uri)
+        uri_dir = os.listdir(url)
         # convert unicode list to printable list
-        for x, word in enumerate(uri_dir):
+        for word in (uri_dir):
             dir_list.append(str(word))
         return dir_list, 'text/plain'
+    elif os.path.isfile(url):
+        uni_url = urllib.pathname2url(url)
+        #url = '/images/sample_1.png'
+        try:
+            obj_file = open(url, 'rb')
+            file_content = obj_file.read()
+            obj_file.close()
+            return file_content, mimetypes.guess_type(uni_url)[0]
+        except:
+            return response_not_found()
+            raise ValueError
+##        except IOError:
+##            raise Exception ("Error reading: can\'t access file.")
+            #return response_not_found()
+    else:
+        #import pdb; pdb.set_trace()
+        #raise ValueError
+        try:
+            return response_not_found(), ''
+        except ValueError:
+            pass
+
+##        except IOError:
+##            raise Exception ("Error reading: can\'t access file.")
+        #except:
+        #    raise Exception ("Error: some problem happened while reading file; not sure what.")
+        #print repr(file_content)
+
+        #import pdb; pdb.set_trace()
+        return file_content, mimetypes.guess_type(uni_url)[0]
+##    else:
+##        return response_not_found()
+##        raise Exception ('404 Not Found')
 
 
-
+def response_not_found():
+    return 'HTTP/1.1 404 Not Found'
+    raise ValueError
 
 
 
@@ -33,15 +79,19 @@ def response_ok(body, mimetype):
     """returns a basic HTTP response"""
     resp = []
     resp.append("HTTP/1.1 200 OK")
-    if mimetype != "":
+    if mimetype == 'image/pjpeg':
+        resp.append('image/jpeg\r\n')
+    elif mimetype == "":
         resp.append("Content-Type: text/plain")
-    else:
-        resp.append(mimetype)
-    resp.append("")
-    if body != "":
+        #resp.append("Content-Type: text/plain or No mimetype found")
+    elif mimetype != "":
+        resp.append("Content-Type: " + mimetype + '\r\n')
+    if body != "" and body:
         resp.append(body)
     else:
         resp.append("this is a pretty minimal response")
+    #resp.append(mimetype)
+    #resp.append("")
     return "\r\n".join(resp)
 
 
@@ -85,11 +135,28 @@ def server():
                         break
 
                 try:
-                    parse_request(request)
+                    uri = parse_request(request)
                 except NotImplementedError:
                     response = response_method_not_allowed()
+                    #raise NotImplementedError (response)
+                    print (response)
                 else:
-                    response = response_ok()
+                    try:
+                        #import pdb; pdb.set_trace()
+                        #if content != '' and mimetype != '':
+                        content, mimetype = resolve_uri(uri)
+                    except ValueError:
+                        #raise ValueError('404 Not Found')
+                        print ('404 Not Found')
+                    #response = response_ok()
+                    if content != '':
+                        if content != 'HTTP/1.1 404 Not Found':
+                            response = response_ok(str(content), str(mimetype))
+                        else:
+                            response = str(content) + ',' + str(mimetype) + '\r\n\r\n404 ' + uri + ' not found.'
+                            #raise ValueError
+                    else:
+                        response = str(content) + ',' + str(mimetype)
 
                 print >>sys.stderr, 'sending response'
                 conn.sendall(response)
